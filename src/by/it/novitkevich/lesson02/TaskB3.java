f2ac35a47ae4643cbfec38147feef7bc18067eec
novitkevich <njelena@gmail.com>
2018-12-06 14:47:57 +0300

    Lesson 2 B

--- src/by/it/novitkevich/lesson02/TaskB3.java
+++ src/by/it/novitkevich/lesson02/TaskB3.java
@@ -11,13 +11,13 @@ package by.it.novitkevich.lesson02;
 class TaskB3 {
         public static void main(String[] args) {
 //        String s = "C";
-//        System.out.println("Новым Годом");
-//        System.out.println("C Новым");
-//        System.out.println("Годом");
+//        System.out.println
+//        System.out.println
+//        System.out.println
 //        System.out.print("C Новым");
-//        System.out.println("Годом");
-//        System.out.println(s);
-//        System.out.print("C Новым!");
+//        System.out.println
+//        System.out.println
+//        System.out.print
 //        System.out.print(" ");
 //        System.out.println("Годом");
     }
